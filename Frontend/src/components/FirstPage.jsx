abb9c89cd6682baf753544e3154a4e275434bed3
Barnali Sarker Shoumita <barnali.cse59@gmail.com>
2024-02-17 01:48:26 +0600

    myyyyyyyyyyyyyyyyyy

--- Frontend/src/components/FirstPage.jsx
+++ Frontend/src/components/FirstPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const FirstPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold mb-8">Welcome to my first website</h1>
+            <h1 className="text-3xl font-bold mb-8">Welcome</h1>
             <div className="flex space-x-4">
                 <Link to="/login">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">Login</button>
